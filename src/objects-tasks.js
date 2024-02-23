47bdd6543e2eef1ac8e645915ac2ffd76bcc74b8
Natallia <natallia.kapitan@gmail.com>
2024-02-23 20:53:21 +0300

    feat: complete task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -240,8 +240,8 @@ function Rectangle(width, height) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  return JSON.stringify(obj);
 }
 
 /**
@@ -255,8 +255,13 @@ function getJSON(/* obj */) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const obj = Object.create(proto);
+  const parsedData = JSON.parse(json);
+
+  Object.assign(obj, parsedData);
+
+  return obj;
 }
 
 /**
